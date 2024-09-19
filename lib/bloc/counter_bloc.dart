c15135626428df21e16df1554efaa380e45dac52
sanchith-c-s <sanchithcs@gmail.com>
2024-09-19 11:18:35 +0530

    first commit

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:bloc_sample/bloc/counter_event.dart';
+import 'package:bloc_sample/bloc/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvent,CounterState>
+{
+  CounterBloc():super(CounterState(count: 0)){
+
+    on<CounterIncrementEvent>((event, emit)=> emit(CounterState(count: state.count +1)));
+
+    on<CounterDecrementEvent>((event, emit)=> emit(CounterState(count: state.count -1)));
+  }
+
+  
+  
+}
---
+++ lib/bloc/counter_event.dart
@@ -0,0 +1,11 @@
+sealed class CounterEvent {
+   
+}
+
+final class CounterIncrementEvent extends CounterEvent {
+  
+}
+
+final class CounterDecrementEvent extends CounterEvent{
+
+}
